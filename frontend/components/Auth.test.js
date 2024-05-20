0a458e8ea3316ba5e1acc79a8cb0deb2ebd7b2d0
DrSumner <157843168+DrSumner@users.noreply.github.com>
2024-05-20 14:15:49 -0400

    finito

--- frontend/components/Auth.test.js
+++ frontend/components/Auth.test.js
@@ -41,30 +41,46 @@ describe('Auth component', () => {
     // ✨ assert that the input has the value entered (done for you)
     expect(userInput).toHaveValue('gabe')
     // ✨ type some text in the password input
+    await user.type(passInput, '12345')
     // ✨ assert that the input has the value entered
-    expect(true).toBe(false) // DELETE
+    expect(passInput).toHaveValue('12345')
+    
   })
   test('[2] Submitting form clicking button shows "Please wait..." message', async () => {
     // ✨ type whatever values on username and password inputs
+    await user.type(userInput, 'gabe')
+    await user.type(passInput, '12345')
     // ✨ click the Login button
+    await user.click(loginBtn)
     // ✨ assert that the "Please wait..." message is visible in the DOM
-    expect(true).toBe(false) // DELETE
+    expect(screen.getByText("Please wait..."))
+    
   })
   test('[3] Submitting form typing [ENTER] shows "Please wait..." message', async () => {
     // ✨ type whatever values in username and password inputs
+    await user.type(userInput, 'gabe')
+    await user.type(passInput, '12345')
     // ✨ hit the [ENTER] key on the keyboard
+    await user.keyboard('[Enter]')
     // ✨ assert that the "Please wait..." message is visible in the DOM
-    expect(true).toBe(false) // DELETE
+    expect(screen.getByText("Please wait..."))
+    
   })
   test('[4] Submitting an empty form shows "Invalid Credentials" message', async () => {
     // ✨ submit an empty form
+    
+    await user.keyboard('[Enter]')
     // ✨ assert that the "Invalid Credentials" message eventually is visible
-    expect(true).toBe(false) // DELETE
+    expect(screen.findByText('Invalid Credentials'))
+    
   })
   test('[5] Submitting incorrect credentials shows "Invalid Credentials" message', async () => {
     // ✨ type whatever username and password and submit form
+    await user.type(userInput, 'gabe')
+    await user.type(passInput, '12345')
+    await user.keyboard('[Enter]')
     // ✨ assert that the "Invalid Credentials" message eventually is visible
-    expect(true).toBe(false) // DELETE
+    expect(screen.findByText('Invalid Credentials'))
   })
   for (const usr of registeredUsers) {
     test(`[6.${usr.id}] Logging in ${usr.username} makes the following elements render:
@@ -72,18 +88,29 @@ describe('Auth component', () => {
         - correct user info (ID, username, birth date)
         - logout button`, async () => {
       // ✨ type valid credentials and submit form
+      await user.type(userInput, usr.username)
+      await user.type(passInput, usr.password)
+      await user.keyboard('[Enter]')
       // ✨ assert that the correct welcome message is eventually visible
+      expect(screen.findByText(`Welcome back, ${usr.username}. We LOVE you!`))
       // ✨ assert that the correct user info appears is eventually visible
+      expect(screen.findByText(`ID: ${usr.id}, Username: ${usr.username}, Born: ${usr.born}`))
       // ✨ assert that the logout button appears
-      expect(true).toBe(false) // DELETE
+      expect(screen.findByTestId('logoutBtn'))
     })
   }
   test('[7] Logging out a logged-in user displays goodbye message and renders form', async () => {
     // ✨ type valid credentials and submit
+    await user.type(userInput, `Shakira`)
+      await user.type(passInput, `Suerte1977%`)
+      await user.keyboard('[Enter]')
     // ✨ await the welcome message
+    await screen.findByText(`Welcome back, Shakira. We LOVE you!`)
     // ✨ click on the logout button (grab it by its test id)
+    await user.click(screen.getByTestId('logoutBtn'))
     // ✨ assert that the goodbye message is eventually visible in the DOM
+    expect(await screen.findByText(`Bye! Please, come back soon.`))
     // ✨ assert that the form is visible in the DOM (select it by its test id)
-    expect(true).toBe(false) // DELETE
+    expect(screen.findByTestId('loginForm'))
   })
 })
